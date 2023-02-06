deaeaadbf45da27495729fa5c4783336d331a1b5
0b1kn00b <polysemantic@gmail.com>
2023-02-06 13:32:58 +0100

    Added python to sanity test

--- stx/i/run/StxWorkspaceSanity.hx
+++ stx/i/run/StxWorkspaceSanity.hx
@@ -4,6 +4,8 @@ class StxWorkspaceSanity{
   static public function main(){
     Sys.println('haxe --run $$STX_WORKSPACE_DIR/stx/i/run/StxWorkspaceSanity.hx');
     Sys.println('________________________________________________________________________________');
+    Sys.print('python ');
+    Sys.command("python --version");
     Sys.print('haxe ');
     Sys.command("haxe --version");
     Sys.print('neko ');
