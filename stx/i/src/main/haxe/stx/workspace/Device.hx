55e0d401030ecbd9a9f35a7a7cade8f80e72ce76
0b1kn00b <polysemantic@gmail.com>
2023-02-22 13:17:03 +0100

    rust is pretty lightweight

---
+++ stx/i/src/main/haxe/stx/workspace/Device.hx
@@ -0,0 +1,9 @@
+package stx.workspace;
+
+typedef Architecture = stx.workspace.device.Architecture;
+typedef OsId          = stx.workspace.device.OsId;
+
+
+class Device{
+  
+}
---
+++ stx/i/src/main/haxe/stx/workspace/device/OsApi.hx
@@ -0,0 +1,5 @@
+package stx.workspace.device;
+
+interface OsApi{
+  public function getOs():OsId;
+}
