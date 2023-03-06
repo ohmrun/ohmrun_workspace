6ce5a841c79e66843218b360f6acf9c9ef49d159
0b1kn00b <polysemantic@gmail.com>
2023-03-06 15:35:08 +0100

    Fixed StxWorkspaceSanity

--- .local/stx/i/run/StxWorkspaceSanity.hx
+++ .local/stx/i/run/StxWorkspaceSanity.hx
@@ -2,7 +2,7 @@ package stx.i.run;
 
 class StxWorkspaceSanity{
   static public function main(){
-    Sys.println('haxe --run $$STX_WORKSPACE_DIR/stx/i/run/StxWorkspaceSanity.hx');
+    Sys.println('haxe --run $$STX_WORKSPACE_DIR/.local/stx/i/run/StxWorkspaceSanity.hx');
     Sys.println('________________________________________________________________________________');
     Sys.print('python ');
     Sys.command("python --version");
@@ -10,7 +10,8 @@ class StxWorkspaceSanity{
     Sys.command("haxe --version");
     Sys.print('neko ');
     Sys.command("neko -version");
-    Sys.print('node ');
-    Sys.command("node --version");
+    Sys.command("plz --version");
+    //Sys.print('bb ');
+    Sys.command("bb --version");
   }
 }
